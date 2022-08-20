bdafd298c12c85a9f27d17e6ede4fe8cac997db2
Faudrey28 <focant.audrey@gmail.com>
2022-08-19 20:24:42 -0700

    Updates

---
+++ server/chunks/713.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.id = 713;
+exports.ids = [713];
+exports.modules = {
+
+/***/ 3713:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/tindog.92f1025d.webp","height":1409,"width":2875,"blurDataURL":"data:image/webp;base64,UklGRk4AAABXRUJQVlA4IEIAAADQAQCdASoIAAQAAkA4JZgCdAEUowugAAD+86/+OHc1z1f2uwDNgczHRgUU7/f8gG4/4XfmcNvnQbj/g5MU9mAAAAA="});
+
+/***/ })
+
+};
+;
