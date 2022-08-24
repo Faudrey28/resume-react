59e161168048f73ebd1bd63af75e3945c0c1aae9
Faudrey28 <focant.audrey@gmail.com>
2022-08-23 20:48:12 -0700

    setup gh-pages to deploy the website to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: "https://audreyfocant.com/",
+  images: {
+    loader: "akamai",
+    path: "",
+  }
 }
 
 module.exports = nextConfig
